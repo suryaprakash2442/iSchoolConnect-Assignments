337da6766b32cad1a3522e6d7ebfe073b41e2f59
suryaprakash2442 <73881606+suryaprakash2442@users.noreply.github.com>
2022-03-28 16:39:49 +0530

    Add files via upload

---
+++ day18-typescript/assignment-bootstrap-site/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--  <app-header></app-header>, -->
+    <app-main></app-main>
+    <!-- <app-footer></app-footer>  -->
+    
+
+  `
+})
+export class AppComponent {
+  title = 'this is my step2 in angular';
+  ischool = "redbox"
+}
---
+++ day18-typescript/assignment-bootstrap-site/src/app/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : 'app-footer',
+    template : `
+    <footer class="pt-3 mt-4 text-muted border-top">
+      &copy; 2021
+    </footer>
+    `
+})
+
+export class FooterComponent{
+    title = 'step2';
+}
